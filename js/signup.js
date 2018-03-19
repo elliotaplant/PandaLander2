9395074798d4e5f410bf46a78be396fa983c698b
Elliot Plant <elliotaplant@gmail.com>
2018-03-19 00:40:30 -0700

    Get styles gang

--- js/signup.js
+++ js/signup.js
@@ -5,28 +5,36 @@ var elements = stripe.elements();
 // Custom styling can be passed to options when creating an Element.
 var style = {
   base: {
-    iconColor: '#70C9FF',
-    color: '#fff',
-    fontWeight: 500,
-    fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
-    fontSize: '15px',
-    fontSmoothing: 'antialiased',
+    iconColor: '#444',
+    color: '#000',
+    fontSize: '16px',
+    lineHeight: '24px',
+    fontFamily: 'Muli, Helvetica, Arial',
 
     ':-webkit-autofill': {
-      color: '#fce883',
+      color: '#fce883'
     },
     '::placeholder': {
-      color: '#DDD',
-    },
+      color: '#6c757d'
+    }
   },
   invalid: {
-    iconColor: '#FFC7EE',
-    color: '#FFC7EE',
-  },
+    iconColor: '#fa755a',
+    color: '#fa755a'
+  }
+};
+
+var classes = {
+  focus: 'focus',
+  empty: 'empty',
+  invalid: 'invalid',
 };
 
 // Create an instance of the card Element.
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {
+  style: style,
+  classes: classes
+});
 
 // Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
@@ -36,10 +44,14 @@ card.addEventListener('change', function(event) {
   var displayError = document.getElementById('card-errors');
   if (event.error) {
     displayError.textContent = event.error.message;
-    displayError.classList.remove('hidden');
+    displayError
+      .classList
+      .remove('hidden');
   } else {
     displayError.textContent = 'No Card Errors';
-    displayError.classList.add('hidden');
+    displayError
+      .classList
+      .add('hidden');
   }
 });
 
@@ -48,16 +60,18 @@ var signupForm = document.getElementById('signup-form');
 signupForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the customer that there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to server.
-      stripeTokenHandler(result.token);
-    }
-  });
+  stripe
+    .createToken(card)
+    .then(function(result) {
+      if (result.error) {
+        // Inform the customer that there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to server.
+        stripeTokenHandler(result.token);
+      }
+    });
 });
 
 function stripeTokenHandler(token) {
@@ -73,3 +87,16 @@ function stripeTokenHandler(token) {
   signupForm.submit();
 }
 // Also should try to get phone number from hash
+$('#signup-form').submit(function(event) {
+  event.preventDefault();
+  var signupForm = $(this);
+  console.log(signupForm.serialize());
+  $
+    .post(signupFormattr('action'), signupForm.serialize())
+    .then(function() {
+      window.location.pathname = 'stillbuilding.html'
+    })
+    .catch(function() {
+      window.location.pathname = 'stillbuilding.html'
+    });
+});
